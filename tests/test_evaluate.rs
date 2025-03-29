ee3dfd3ef45e0bff6de28f089f5b6d1bb922c5f9
jannabiforever <jungini1226@gmail.com>
2025-03-29 14:31:57 +0900

    test

---
+++ tests/test_evaluate.rs
@@ -0,0 +1,28 @@
+use codecrafters_interpreter::lox_evaluate;
+use std::process::ExitCode;
+
+macro_rules! evaluate_test {
+    ($src:expr, exit_code = $exit_code:expr, stdout = $stdout:expr, stderr = $stderr:expr) => {
+        let mut ok_buf = Vec::new();
+        let mut err_buf = Vec::new();
+
+        let exit_code = lox_evaluate($src, &mut ok_buf, &mut err_buf, false);
+
+        assert_eq!(exit_code, ExitCode::from($exit_code));
+        assert_eq!(String::from_utf8(ok_buf).unwrap().trim(), $stdout.trim());
+        assert_eq!(String::from_utf8(err_buf).unwrap().trim(), $stderr.trim());
+    };
+    ($src:expr, stdout = $stdout:expr) => {
+        parse_test! {
+            $src,
+            exit_code = 0,
+            stdout = $stdout,
+            stderr = ""
+        };
+    };
+}
+
+/// Literals: Booleans & Nil
+/// https://app.codecrafters.io/courses/interpreter/stages/iz6
+#[test]
+fn iz6() {}
