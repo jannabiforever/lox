24be924b75054a2857a9a88878e240dbb86b9cc1
jannabiforever <jungini1226@gmail.com>
2025-04-01 17:54:58 +0900

    Improved TokenStream logic

--- src/tokenize/token_stream.rs
+++ src/tokenize/token_stream.rs
@@ -1,45 +1,43 @@
 use super::Token;
 
+enum TokenStreamState<'a> {
+    /// Index of the next token to be returned.
+    NotExpired(usize),
+    /// The stream has expired, and the last token(eof) is stored here.
+    Expired(&'a Token<'a>),
+}
+
 pub struct TokenStream<'a> {
     tokens: &'a [Token<'a>],
-    current: usize,
-    eof: Option<&'a Token<'a>>,
     line: usize,
+    state: TokenStreamState<'a>,
 }
 
 impl<'a> TokenStream<'a> {
     pub fn new(tokens: &'a [Token<'a>]) -> Self {
         Self {
             tokens,
-            current: 0,
-            eof: None,
+            state: TokenStreamState::NotExpired(0),
             line: 0,
         }
     }
 
     pub fn next(&mut self) -> &'a Token<'a> {
-        self.assert_if_expired_then_eof();
-
-        if self.expired() {
-            self.eof.as_ref().unwrap()
-        } else {
-            let token = &self.tokens[self.current];
-            self.line = token.line;
-            self.current += 1;
-            if self.expired() {
-                self.eof = Some(token);
+        match self.state {
+            TokenStreamState::NotExpired(index) => {
+                let token = &self.tokens[index];
+                self.line = token.line;
+                self.set_state(index + 1);
+                token
             }
-            token
+            TokenStreamState::Expired(token) => token,
         }
     }
 
     pub fn peek(&self) -> &'a Token<'a> {
-        self.assert_if_expired_then_eof();
-
-        if self.expired() {
-            self.eof.as_ref().unwrap()
-        } else {
-            &self.tokens[self.current]
+        match self.state {
+            TokenStreamState::NotExpired(index) => &self.tokens[index],
+            TokenStreamState::Expired(token) => token,
         }
     }
 
@@ -47,22 +45,11 @@ impl<'a> TokenStream<'a> {
         self.line
     }
 
-    fn expired(&self) -> bool {
-        self.current >= self.tokens.len()
-    }
-
-    // Note: Could be done with XOR, but this is more readable.
-    fn assert_if_expired_then_eof(&self) {
-        if self.current >= self.tokens.len() {
-            assert!(
-                self.eof.is_some(),
-                "TokenStream has expired, but eof is None"
-            );
+    fn set_state(&mut self, index: usize) {
+        if index < self.tokens.len() {
+            self.state = TokenStreamState::NotExpired(index);
         } else {
-            assert!(
-                self.eof.is_none(),
-                "TokenStream has not expired, but eof is Some"
-            )
+            self.state = TokenStreamState::Expired(&self.tokens[self.tokens.len() - 1]);
         }
     }
 }
